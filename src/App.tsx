d14574ab2e534246e3163a70f5ad015316708107
Akram Gafurov <akramminsk@yandex.by>
2022-10-03 00:22:25 +0300

    feat: add fish text

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolor nostrum quas laudantium omnis perferendis illo ullam eos, placeat illum facere, sit reiciendis explicabo excepturi consequatur aliquam debitis minus alias!
+        Cumque itaque, eaque reiciendis voluptas natus tempore corrupti id culpa commodi deleniti mollitia ipsum ea, nulla voluptates, quidem iusto ducimus voluptatem delectus. Commodi labore maiores quod, sapiente numquam illo fuga.
+        Possimus voluptate recusandae in placeat nemo autem, cumque atque deserunt sit dolore quaerat officiis suscipit facilis consequatur doloribus expedita magnam laudantium earum vel minus corporis qui? Iusto voluptate rem blanditiis.</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
