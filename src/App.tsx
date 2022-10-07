3df814c492985ed0612c3b5ae699464e98b64197
Akram Gafurov <akramminsk@yandex.by>
2022-10-08 00:07:04 +0300

    Revert "Revert "feat: add fish text""
    This reverts commit 1382a87ca1c0805c761ee99a52d33fc6302c24a6.

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Lorem  ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolor nostrum quas laudantium omnis perferendis illo ullam eos, placeat illum facere, sit reiciendis explicabo excepturi consequatur aliquam debitis minus alias!
+        Cumque itaque, eaque reiciendis voluptas natus tempore corrupti id culpa commodi deleniti mollitia ipsum ea, nulla voluptates, quidem iusto ducimus voluptatem delectus. Commodi labore maiores quod, sapiente numquam illo fuga.
+        Possimus voluptate recusandae in placeat nemo autem, cumque atque deserunt sit dolore quaerat officiis suscipit facilis consequatur doloribus expedita magnam laudantium earum vel minus corporis qui? Iusto voluptate rem blanditiis.</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and  save to reload.
+          33
+          333
+
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs2.org"
           target="_blank"
           rel="noopener noreferrer"
         >
